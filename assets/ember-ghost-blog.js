ffafdd5f4a5aa0be2abd8b0c0ae2cae4c3badbaf
Yihan Liu <45199394+astrid-yihan@users.noreply.github.com>
2019-01-25 18:51:23 +0800

    Updates

--- assets/ember-ghost-blog.js
+++ assets/ember-ghost-blog.js
@@ -2739,7 +2739,7 @@ catch(err) {
 ;
 if (typeof FastBoot === 'undefined') {
   if (!runningTests) {
-    require('ember-ghost-blog/app')['default'].create({"name":"ember-ghost-blog","version":"0.0.0+8776cd97"});
+    require('ember-ghost-blog/app')['default'].create({"name":"ember-ghost-blog","version":"0.0.0+f75b3c5e"});
   }
 }
 
